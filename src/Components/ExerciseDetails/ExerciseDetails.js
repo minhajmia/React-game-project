6056c838b96533430b77da239501e8575f81cb77
minhajmia <minhajmia418@gmail.com>
2022-09-29 02:10:22 +0600

    all name completed

--- src/Components/ExerciseDetails/ExerciseDetails.js
+++ src/Components/ExerciseDetails/ExerciseDetails.js
@@ -21,9 +21,9 @@ const ExerciseDetails = ({ cart, breakTime }) => {
 
   return (
     <div className="ExerciseDetails-container mx-5 mt-5">
-      <h2 className="text-left capitalize font-semibold">Exercise Details</h2>
+      <h2 className="text-left capitalize font-semibold">Game Details</h2>
       <div className="exercise-time">
-        <span>Exercise time </span>
+        <span>Game time </span>
         <span>{total} seconds</span>
       </div>
       <div className="break-time">
